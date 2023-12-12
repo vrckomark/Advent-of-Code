c4ae387a0ee3fd1d8429bb18d24c56f8cd1451a8
Vrechko <vrckomark@gmail.com>
2023-12-12 15:29:00 +0100

    day 5 part 1 done

--- 2023/day5/part1.py
+++ 2023/day5/part1.py
@@ -1,47 +1,47 @@
 from time import perf_counter
 
+start = perf_counter()
+
 seeds=[]
 maps=[]
 with open("input.txt","r") as f:
     current_map=[]
-
-    start = perf_counter()
-    for line in f.readlines():
+    lines = f.readlines()
+    for i,line in enumerate(lines):
         if "seeds" in line:
             seeds=[int(i) for i in list(filter(None, line.strip("\n").split(":")[1].split(" ")))]
         if line[0].isnumeric():
             current_row=[int(i) for i in line.strip("\n").split(" ")]
             current_map.append(current_row)
-        if line=="\n":
+        if line=="\n" or i==len(lines)-1:
             maps.append(current_map)
             current_map=[]
 
 locations=[]
 
-# for seed in seeds:
-#     seed_result=seed
-#     for map_ in maps:
-#         current_src=seed_result
-#         for row in map_:
-#             dst_min=row[0]
-#             src_min = row[1]
-#             map_range=row[2]
-#             if current_src<src_min:
-#                 continue
-#             if current_src <= (current_src+map_range) and current_src>=src_min:
-#                 seed_result= dst_min+ current_src-src_min
-#     locations.append(seed_result)
 
-# print(f"MIN:\n\n{min(locations)}")
+for seed in seeds:
+    destination=seed
+    for map_ in maps:
+        source=destination
+        for row in map_:
+            destination_min=row[0]
+            source_min = row[1]
+            map_range=row[2]
+            if source >= source_min and source < source_min+map_range:
+                destination = source - source_min + destination_min
+                break
+            else:
+                destination=source
+    locations.append(destination)
 
-for i in maps:
-    for row in i:
-        print(i)
+print(locations)
+
+print(f"MIN:\n\n{min(locations)}")
+
 
 end = perf_counter()
 
+
 print(f"{round(((end-start)*1000),2)}ms")
-# for map_ in maps:
-#     for row in map_:
-#         print(row)
-#     print("\n\n")
+
